4832a5874af989c4361c71392e43abb849d90c25
hasantezcan <hasantezcan77@gmail.com>
2020-12-27 09:45:18 +0300

    Add day 3 materials

---
+++ _data/practice/day3/1-jquery-vs-react/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(1);
+
+	return (
+		<div className="App">
+			<h1>{count}</h1>
+			<button onClick={() => setCount(2)}>Click</button>
+		</div>
+	);
+}
+
+export default App;
